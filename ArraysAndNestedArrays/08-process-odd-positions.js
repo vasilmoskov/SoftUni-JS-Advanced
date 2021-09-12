3313d0aa28c0eae83a400d4f18cc79f7ebd3446b
Vasil Moskov <moskov.vasil@gmail.com>
2021-09-12 14:07:57 +0300

    Delete ArraysAndNestedArrays directory

--- ArraysAndNestedArrays/08-process-odd-positions.js
+++
@@ -1,9 +0,0 @@
-function solution(arr) {
-    return output = arr
-        .filter((a, i) => i % 2 !== 0)
-        .map(e => e * 2)
-        .reverse()
-        .join(' ');
-}
-
-console.log(solution([3, 0, 10, 4, 7, 3]));
